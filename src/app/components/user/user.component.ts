9adcf47e0376c30029fee7c93c2bdf70e5ae72e8
brunaportelaa <brunaporteladj@gmail.com>
2024-09-12 12:30:19 -0300

    Added @Input decorators to the user component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, Input, signal } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
+
 
 @Component({
   selector: 'app-user',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+
 export class UserComponent {
+  
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectedUser() {
+    
+  }
+
 
 }
