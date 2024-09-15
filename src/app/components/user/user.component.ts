a023e15d5efbd4eb042e7e10d662368011f32216
brunaportelaa <brunaporteladj@gmail.com>
2024-09-15 16:37:56 -0300

    Added the Output decorator to send user id from user component to app component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,6 +1,4 @@
-import { Component, computed, input, Input, signal } from '@angular/core';
-import { DUMMY_USERS } from '../../dummy-users';
-
+import { Component,  EventEmitter,  Input, Output, } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -12,15 +10,18 @@ import { DUMMY_USERS } from '../../dummy-users';
 
 export class UserComponent {
   
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar
-  }
+  @Output() select = new EventEmitter();
 
-  onSelectedUser() {
-    
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  };
+
+  onSelectUser() {
+    this.select.emit(this.id)
   }
 
 
