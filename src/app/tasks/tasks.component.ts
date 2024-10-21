c89e2a7e735978ffd7b908d9f83c91a9484e476a
brunaportelaa <brunaporteladj@gmail.com>
2024-10-20 21:38:07 -0300

    Starting project for module on debugging

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,27 +1,25 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTask } from './task/task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
+  imports: [TaskComponent, NewTaskComponent],
 })
-
 export class TasksComponent {
-
-  @Input( { required: true } ) userId!: string;
-  @Input( { required: true } ) name!: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
   isAddingTask = false;
 
-  constructor( private tasksService: TasksService ) {}
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasksService.getUserTasks(this.userId) 
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onStartAddTask() {
@@ -31,5 +29,4 @@ export class TasksComponent {
   onCloseAddTask() {
     this.isAddingTask = false;
   }
-
 }
